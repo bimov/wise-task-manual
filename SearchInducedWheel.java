f619f6fe46e89710586193bde48e80dcdc2278d9
Баяндин Дмитрий <116660750+bimov@users.noreply.github.com>
2026-02-28 14:46:52 +0300

    Implement SearchInducedWheel class for graph analysis

---
+++ SearchInducedWheel.java
@@ -0,0 +1,107 @@
+package ru.leti;
+
+import ru.leti.wise.task.graph.model.Edge;
+import ru.leti.wise.task.plugin.graph.GraphProperty;
+import ru.leti.wise.task.graph.model.Graph;
+import ru.leti.wise.task.graph.model.Vertex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Реализует поиск индуцированного колеса в графе
+ */
+public class SearchInducedWheel implements GraphProperty {
+
+    private Graph graph;
+    private ArrayList<Integer> usedId;
+
+    // Возвращает степень поданной вершины графа
+    private int getDegree(Vertex vertex){
+        int degree = 0;
+        List<Edge> edges = graph.getEdgeList();
+        for (Edge edge : edges) {
+            if (edge.getSource() == vertex.getId()){
+                degree++;
+            }
+            if (edge.getTarget() == vertex.getId()){
+                degree++;
+            }
+        }
+        return degree;
+    }
+
+    // Возвращает список соседей вершин поданной вершины
+    private ArrayList<Integer> getNeighbors(Vertex vertex){
+        ArrayList<Integer> neighbors = new ArrayList<Integer>();
+        for (Edge edge : graph.getEdgeList()){
+            if (edge.getSource() == vertex.getId()){
+                neighbors.add(edge.getTarget());
+            }
+            if (edge.getTarget() == vertex.getId()){
+                neighbors.add(edge.getSource());
+            }
+        }
+        return neighbors;
+    }
+
+    // Проверяет необходимое условие существования подграфа колеса с центром
+    // в поданной вершине (deg(v) >= 3)
+    private boolean isCenter(Vertex vertex) {
+        if (getDegree(vertex) < 3)
+            return false;
+        return true;
+    }
+
+    // Возвращает вершину по ее id
+    private Vertex getVertex(int vertexId){
+        for (Vertex vertex : graph.getVertexList()){
+            if (vertex.getId() == vertexId){
+                return vertex;
+            }
+        }
+        return graph.getVertexList().get(0);
+    }
+
+    // Совершает обход в глубину относительно вершин соседей поданной вершины центра
+    // Если во время обхода находиться цикл (вторая встреча вершины) возвращается true
+    // Иначе false
+    private boolean dfs(Vertex vertex, ArrayList<Integer> neighbors, Vertex startVertex, Integer prevVertexId) {
+        if (usedId.contains(vertex.getId()))
+            return true;
+
+        usedId.add(vertex.getId());
+        boolean isFindCycles = false;
+        List<Integer> currentNeighbors = getNeighbors(vertex);
+
+        for (Integer currentNeighbor : currentNeighbors){
+            if (neighbors.contains(currentNeighbor) && !currentNeighbor.equals(prevVertexId)){
+                if (!(vertex.equals(startVertex) && usedId.contains(currentNeighbor))) {
+                    Vertex newVertex = getVertex(currentNeighbor);
+                    isFindCycles |= dfs(newVertex, neighbors, startVertex, vertex.getId());
+                }
+            }
+        }
+        return isFindCycles;
+    }
+
+    // Запускает проверку того, есть ли индуцированное колеса с центром в поданной вершине
+    private boolean checkVertex(Vertex vertex){
+        if (isCenter(vertex)){
+            ArrayList<Integer> neighbors = getNeighbors(vertex);
+            this.usedId = new ArrayList<Integer>();
+            return dfs(vertex, neighbors, vertex, vertex.getId());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean run(Graph graph){
+        this.graph = graph;
+        for (Vertex vertex : graph.getVertexList()) {
+            if (checkVertex(vertex))
+                return true;
+        }
+        return false;
+    }
+}
